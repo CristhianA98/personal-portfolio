59ef94fdb859408a9bf64ca4c59c79d202c07219
CristhianA98 <68231571+CristhianA98@users.noreply.github.com>
2023-03-16 20:53:29 -0500

    Js change

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { Portfolio } from "./Portfolio";
 import "./index.css";
 import "flowbite";
 
+import "./assets/js/scroll.js"
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Portfolio />
